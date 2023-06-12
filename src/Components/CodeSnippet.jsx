c052097859f3460c87ec411bdeb22620e439fad1
uzayapiu <uzayapiu@gmail.com>
2023-06-12 21:39:38 +0300

    upcate

--- src/Components/CodeSnippet.jsx
+++ src/Components/CodeSnippet.jsx
@@ -2,7 +2,7 @@ import { CodeBlock, CopyBlock, dracula } from "react-code-blocks";
 
 export default function CodeSnippet({ codeText }) {
     return (
-        <div className="mb-12 sm:text-base text-[12px] rounded-xl overflow-hidden">
+        <div className="mb-12 sm:text-base text-[8px] rounded-xl overflow-hidden">
             <CodeBlock
                 text={codeText}
                 language="javascript"
